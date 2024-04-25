040f8c6fcc37b4f12dfcbee66fd22c86e1f317dd
Vincenzo Giordano <68223847+vinsgiordi@users.noreply.github.com>
2024-04-25 11:33:54 +0200

    Update server.c

--- Server/server.c
+++ Server/server.c
@@ -1,4 +1,3 @@
-#include <libpq-fe.h>
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
@@ -10,235 +9,257 @@
 #include <netinet/in.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <libpq-fe.h>
 
-struct User {
-  char username[50];
-  char password[50];
-  char accessibility[100];
-  int flag;
+// Costanti
+#define PORT_NUMBER 8080
+#define BUFFER_SIZE 2000
+
+// Struttura dati per gli utenti
+struct User
+{
+    char username[50];
+    char password[50];
+    char accessibility[100];
+    int flag;
 };
 
-PGconn *start_connection(PGconn *conn);
-PGresult *send_query_insert(PGconn *conn, struct User user);
-PGresult *send_query_select(PGconn *conn, struct User *user);
-bool registration(struct User user, PGconn *conn);
-bool login(struct User *user, PGconn *conn);
-void *connection_handler(void *socket_desc);
-void close_connection(PGconn *conn, PGresult *res);
-void sendMessageToClient(char* message, int sock);
+// Prototipi delle funzioni
+PGconn *establishDBConnection(PGconn *connection);
+bool registerUser(struct User user, PGconn *connection);
+bool loginUser(struct User *user, PGconn *connection);
+void *handleConnection(void *socketDesc);
+void sendMessageToClient(char *message, int socket);
 
-int main(int argc, char const *argv[]) {
-  int socket_desc, client_sock, c, *new_sock;
-	struct sockaddr_in server, client;
+int main(void)
+{
+    int serverSocket, clientSocket;
+    struct sockaddr_in serverAddress, clientAddress;
+    socklen_t clientAddressLength = sizeof(struct sockaddr_in);
 
-  // Crea il socket
-	socket_desc = socket(AF_INET , SOCK_STREAM , 0);
-	if (socket_desc == -1) {
-		printf("Errore durante la creazione della socket");
-	}
-
-  // Prepara la struttura sockaddr_in
-	server.sin_family = AF_INET;
-	server.sin_addr.s_addr = INADDR_ANY;
-	server.sin_port = htons(8080);
-
-  // Effettua il binding
-	if(bind(socket_desc, (struct sockaddr *)&server, sizeof(server)) < 0) {
-		// Stampa il messaggio di errore
-		perror("Bind fallito");
-		exit(1);
-	}
-
-  // Metti in ascolto
-	listen(socket_desc, 3);
-
-  printf("\n BENVENUTO SUL SERVER DI MOVIEHUB \n\n");
-
-  // Accetta una connessione in entrata
-	printf("Server in ascolto su porta 8080...\n\n");
-	c = sizeof(struct sockaddr_in);
-
-	while ((client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c))) {
-    printf("Connessione con il client stabilita\n\n");
-
-    pthread_t sniffer_thread;
-		new_sock = malloc(1);
-		*new_sock = client_sock;
-
-    if (pthread_create(&sniffer_thread, NULL, connection_handler, (void*) new_sock) < 0) {
-      perror("Errore durante la creazione del thread\n\n");
-			exit(1);
+    // Creazione del socket del server
+    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverSocket == -1)
+    {
+        perror("Error creating socket");
+        exit(EXIT_FAILURE);
     }
 
-    printf("Handler assegnato\n\n");
-  }
+    // Configurazione dell'indirizzo del server
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    serverAddress.sin_port = htons(PORT_NUMBER);
 
-  if (client_sock < 0) {
-		perror("Connessione con il client fallita\n\n");
-		exit(1);
-	}
+    // Associazione del socket all'indirizzo del server
+    if (bind(serverSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0)
+    {
+        perror("Bind failed");
+        exit(EXIT_FAILURE);
+    }
 
-  return 0;
+    // Ascolto delle connessioni in arrivo
+    listen(serverSocket, 3);
+
+    printf("Server MovieHub online\n");
+    printf("Server in ascolto su porta %d...\n\n", PORT_NUMBER);
+
+    // Accettazione delle connessioni e gestione dei thread
+    while ((clientSocket = accept(serverSocket, (struct sockaddr *)&clientAddress, &clientAddressLength)))
+    {
+        printf("Connection with client established\n");
+
+        pthread_t thread;
+        int *newSocket = malloc(sizeof(int));
+        if (!newSocket)
+        {
+            perror("Error allocating memory");
+            exit(EXIT_FAILURE);
+        }
+        *newSocket = clientSocket;
+
+        if (pthread_create(&thread, NULL, handleConnection, (void *)newSocket) != 0)
+        {
+            perror("Error creating thread");
+            free(newSocket);
+            exit(EXIT_FAILURE);
+        }
+    }
+    if (clientSocket < 0)
+    {
+        perror("Connection with client failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    return 0;
 }
 
-PGconn *start_connection(PGconn *conn) {
-  char *conninfo = "host=localhost dbname=moviehub user=postgres password=moviehub2024";
-  
-  conn = PQconnectdb(conninfo);
+// Connessione al database
+PGconn *establishDBConnection(PGconn *connection)
+{
+    char *connectionInfo = "host=192.168.56.1 port=5432 dbname=moviehub user=postgres password=admin";
+    connection = PQconnectdb(connectionInfo);
 
-  /* Controlla se la connessione è stata effettuata con successo */
-  if (PQstatus(conn) != CONNECTION_OK) {
-      fprintf(stderr, "Connessione al database fallita: %s\n", PQerrorMessage(conn));
-      PQfinish(conn);
-      exit(1);
-  }
+    if (PQstatus(connection) != CONNECTION_OK)
+    {
+        fprintf(stderr, "Database connection failed: %s\n", PQerrorMessage(connection));
+        PQfinish(connection);
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+        printf("Database connection established successfully\n\n");
+    }
 
-  return conn;
+    return connection;
 }
 
-PGresult *send_query_insert(PGconn *conn, struct User user) {
-  char query_insert[1000];
-  sprintf(query_insert, "INSERT INTO users(username, password, accessibility) VALUES ('%s', '%s', '%s')", user.username, user.password, user.accessibility);
+// Registrazione di un nuovo utente
+bool registerUser(struct User user, PGconn *connection)
+{
+    char query[1000];
+    sprintf(query, "INSERT INTO users(username, password, accessibility) VALUES ('%s', '%s', '%s')", user.username, user.password, user.accessibility);
 
-  PGresult *res = PQexec(conn, query_insert);
-  if (PQresultStatus(res) != PGRES_COMMAND_OK) {
-      fprintf(stderr, "Lo statement INSERT ha fallito: %s", PQerrorMessage(conn));
-      PQclear(res);
-      exit(1);
-  }
+    PGresult *result = PQexec(connection, query);
+    if (PQresultStatus(result) != PGRES_COMMAND_OK)
+    {
+        fprintf(stderr, "Error executing query: %s", PQerrorMessage(connection));
+        PQclear(result);
+        exit(EXIT_FAILURE);
+    }
+    PQclear(result);
 
-  PQclear(res);
-  return res;
-}
-
-PGresult *send_query_select(PGconn *conn, struct User *user) {
-  char query_select[1000];
-
-  if (user->flag > 0) {
-    sprintf(query_select, "SELECT accessibility FROM users WHERE username='%s'", user->username);
-  }
-  else {
-    sprintf(query_select, "SELECT accessibility FROM users WHERE username='%s' AND password='%s'", user->username, user->password);
-  }
-
-  PGresult *res = PQexec(conn, query_select);
-  if (PQresultStatus(res) != PGRES_TUPLES_OK) {
-      fprintf(stderr, "Lo statement SELECT ha fallito: %s", PQerrorMessage(conn));
-      PQclear(res);
-      exit(1);
-  }
-
-  int rows = PQntuples(res);
-  if (rows > 0) {
-    strcpy(user->accessibility, PQgetvalue(res, 0, 0));
-  }
-  
-  PQclear(res);
-  return res; 
-}
-
-bool registration(struct User user, PGconn *conn) {
-  PGresult *res = send_query_insert(conn, user);
-  close_connection(conn, res);
-
-  return true;
-}
-
-bool login(struct User *user, PGconn *conn) {
-  PGresult *res = send_query_select(conn, user);
-  close_connection(conn, res);
-
-  int rows = PQntuples(res);
-  if (rows > 0) {
     return true;
-  }
-  else {
-    return false;
-  }
 }
 
-void *connection_handler(void *socket_desc) {
-  // Ottieni il descrittore del socket
-	int sock = *(int*)socket_desc;
-  struct User user;
-	int read_size;
-  char message[255], client_message[2000];
-  PGconn *conn;
-
-  conn = start_connection(conn);
-
-  // Ricevi un messaggio dal client
-	while ((read_size = recv(sock, client_message, 2000, 0)) > 0 ) {
-    char* token = strtok(client_message, ";");
-
-    if (token != NULL) {
-      if (strcmp(token, "Registrazione") == 0) {
-        printf("Registrazione nuovo utente in corso...\n\n");
-
-        token = strtok(NULL, ";");
-        strcpy(user.username, token);
-
-        token = strtok(NULL, ";");
-        strcpy(user.password, token);
-
-        token = strtok(NULL, ";");
-        strcpy(user.accessibility, token);
-
-        if (registration(user, conn)) {
-          printf("Registrazione avvenuta con successo\n\n");
-          strcpy(message, "OK");
-        }
-        else {
-          printf("Errore durante la registrazione\n\n");
-          strcpy(message, "Errore");
-        }       
-      }
-
-      if (strcmp(token, "Login") == 0) {
-        printf("Login utente in corso...\n\n");
-
-        token = strtok(NULL, ";");
-        strcpy(user.username, token);
-
-        token = strtok(NULL, ";");
-        strcpy(user.password, token);
-
-        if (login(&user, conn)) {
-          printf("Login avvenuto con successo\n\n");
-          strcpy(message, user.accessibility);
-        }
-        else {
-          printf("Errore durante il login\n\n");
-          strcpy(message, "Errore");
-        }
-      }
-
-      sendMessageToClient(message, sock); 
+// Login utente
+bool loginUser(struct User *user, PGconn *connection)
+{
+    char querySelect[1000];
+    if (user->flag > 0)
+    {
+        sprintf(querySelect, "SELECT accessibility FROM users WHERE username='%s'", user->username);
     }
-	}
+    else
+    {
+        sprintf(querySelect, "SELECT accessibility FROM users WHERE username='%s' AND password='%s'", user->username, user->password);
+    }
 
-  if (read_size == 0)	{
-		puts("Client disconnesso\n\n");
-		fflush(stdout);
-	}
-	else if (read_size == -1)	{
-		perror("recv failed");
-	}
+    PGresult *result = PQexec(connection, querySelect);
+    if (PQresultStatus(result) != PGRES_TUPLES_OK)
+    {
+        fprintf(stderr, "Error executing query: %s", PQerrorMessage(connection));
+        PQclear(result);
+        exit(EXIT_FAILURE);
+    }
 
-	// Libera il puntatore al socket
-	free(socket_desc);
-
-  return 0;
+    int rows = PQntuples(result);
+    if (rows > 0)
+    {
+        // Se l'utente è stato trovato, aggiorna la sua accessibilità
+        strcpy(user->accessibility, PQgetvalue(result, 0, 0));
+        PQclear(result);
+        return true;
+    }
+    else
+    {
+        PQclear(result);
+        return false;
+    }
 }
 
-void sendMessageToClient(char* message, int sock) {
-  if (write(sock, message, strlen(message)) < 0) {
-    perror("Errore durante la scrittura sul socket\n\n");
-	  exit(1);
-  }
+// Funzione per gestire la connessione di un client
+void *handleConnection(void *socketDesc)
+{
+    int clientSocket = *(int *)socketDesc;
+    struct User user;
+    char message[255], clientMessage[BUFFER_SIZE];
+    PGconn *connection = establishDBConnection(connection);
+
+    // Ricevi un messaggio dal client
+    while (recv(clientSocket, clientMessage, sizeof(clientMessage), 0) > 0)
+    {
+        char *token = strtok(clientMessage, ";");
+
+        if (token != NULL)
+        {
+            if (strcmp(token, "Registrazione") == 0)
+            {
+                printf("Register new user...\n");
+
+                token = strtok(NULL, ";");
+                strcpy(user.username, token);
+
+                token = strtok(NULL, ";");
+                strcpy(user.password, token);
+
+                token = strtok(NULL, ";");
+                strcpy(user.accessibility, token);
+
+                if (registerUser(user, connection))
+                {
+                    printf("Registration was successful\n\n");
+                    strcpy(message, "OK");
+                }
+                else
+                {
+                    printf("Error during registration\n\n");
+                    strcpy(message, "Errore");
+                }
+            }
+
+            if (strcmp(token, "Login") == 0)
+            {
+                printf("Login utente ...\n\n");
+
+                token = strtok(NULL, ";");
+                strcpy(user.username, token);
+
+                token = strtok(NULL, ";");
+                strcpy(user.password, token);
+
+                if (loginUser(&user, connection))
+                {
+                    printf("Login successful\n\n");
+                    strcpy(message, user.accessibility);
+                }
+                else
+                {
+                    printf("Error during login\n\n");
+                    strcpy(message, "Errore");
+                }
+            }
+
+            sendMessageToClient(message, clientSocket);
+        }
+    }
+
+    // Gestione della disconnessione del client
+    ssize_t recvResult = recv(clientSocket, message, sizeof(message), 0);
+    if (recvResult == 0)
+    {
+        puts("Client disconnected\n\n");
+        fflush(stdout);
+        close(clientSocket);
+    }
+    else if (recvResult == (ssize_t)-1)
+    {
+        perror("recv failed");
+        close(clientSocket);
+    }
+
+    // Chiusura del socket e liberazione delle risorse
+    close(clientSocket);
+    PQfinish(connection);
+    free(socketDesc);
+    return 0;
 }
 
-void close_connection(PGconn *conn, PGresult *res) {
-    PQclear(res);
-    PQfinish(conn);
+// Funzione per inviare un messaggio al client
+void sendMessageToClient(char *message, int sock)
+{
+    if (write(sock, message, strlen(message)) < 0)
+    {
+        perror("Error writing to socket\n\n");
+        exit(EXIT_FAILURE);
+    }
 }
